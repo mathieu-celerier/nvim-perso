acf81bc214b0390db2fbac45120258a09ccc61a6
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-08-17 13:28:21 +0900

    Update config

--- lua/mathieu/plugins/git-sync.lua
+++ lua/mathieu/plugins/git-sync.lua
@@ -6,6 +6,11 @@ return {
 				path = "~/Documents/obsidian-notes",
 				sync_interval = 5,
 				commit_template = "vault sync: {timestamp}",
+				auto_pull = true, -- Pull before pushing (default: true)
+				auto_push = true, -- Push after committing (default: true)
+				add_all = true,
+				pull_before_push = true,
+				handle_conflicts = "pause",
 			},
 		},
 	},
